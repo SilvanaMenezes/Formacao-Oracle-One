da72f48d898f278cbc7302c84cf4980f54404320
Silvana de Menezes <silvanasales.menezes@gmail.com>
2022-04-16 21:35:42 -0300

    validando o restante dos campos

--- javascript/js/form.js
+++ javascript/js/form.js
@@ -8,10 +8,13 @@ botaoAdicionar.addEventListener("click", function(event){
 
   //CRIA A TR E A TD DO PACIENTE 
   var pacienteTr = montaTr(paciente);
+
+  var erros = validaPaciente(paciente);
+  console.log(erros);
   
-  if(!validaPaciente(paciente)){
-    console.log("Paciente inválido");
-    return
+  if(erros.length > 0){
+    exibeMensagensDeErro(erros);
+    return;
   }
 
   //ADICIONANDO O PACIENTE NA TABELA
@@ -19,7 +22,21 @@ botaoAdicionar.addEventListener("click", function(event){
   tabela.appendChild(pacienteTr);
   form.reset();
 
+  var mensagensErro = document.querySelector("#mensagens-erro");
+  mensagensErro.innerHTML = "";
+
 });
+
+function exibeMensagensDeErro(erros){
+  var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = "";
+
+  erros.forEach(function(erro){
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);    
+  });  
+}
 
 function obtemPacienteDoFormulario (form){
   var paciente = {
@@ -54,9 +71,27 @@ function montaTd(dado,classe){
 }
 
 function validaPaciente(paciente){
-  if(validaPeso(paciente.peso)){
-    return true;
-  }else{
-    return false;
+  var erros = [];
+
+  if(paciente.nome.length == 0){
+    erros.push("O nome não pode ser em branco")
   }
+  if(!validaPeso(paciente.peso)){
+     erros.push("Peso é Inválido");
+  }  
+  if(!validaAltura(paciente.altura)){
+     erros.push("Altura é Inválida");
+  }
+  if(paciente.gordura.length == 0){
+    erros.push("A gordura não pode ser em branco");
+  }
+  if(paciente.peso.length == 0){
+    erros.push("O peso não pode ser em branco");
+  }
+  if(paciente.altura.length == 0){
+    erros.push("A altura não pode ser em branco");
+  }
+
+
+  return erros;
 }
