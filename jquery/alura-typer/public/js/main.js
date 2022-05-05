8e44db3b3e5dc44e3b02c9b34e0636735a1b3efc
Silvana de Menezes <silvanasales.menezes@gmail.com>
2022-05-05 13:35:25 -0300

    projeto final

--- jquery/alura-typer/public/js/main.js
+++ jquery/alura-typer/public/js/main.js
@@ -19,10 +19,28 @@ function atualizaTamanhoFrase(){
 function inicializaContadores(){
   campo.on("input",function(){
     var conteudo = campo.val();
+
     var qtdPalavras = conteudo.split(/\S+/).length -1;
     $("#contador-palavras").text(qtdPalavras);
+
     var qtdCaracteres = conteudo.length;
     $("#contador-caracteres").text(qtdCaracteres);
+  });
+}
+
+function inicializaMarcadores(){
+  var frase = $(".frase").text();
+  campo.on("input",function(){
+    var digitado = campo.val();
+    var comparavel = frase.substr(0, digitado.length);
+        
+    if(digitado == comparavel){
+      campo.addClass("borda-verde");
+      campo.removeClass("borda-vermelha");
+    }else{
+      campo.addClass("borda-vermelha");
+      campo.removeClass("borda-verde");
+    }
   });
 }
 
@@ -44,51 +62,14 @@ function finalizaJogo(){
   campo.attr("disabled",true);        
   campo.toggleClass("campo-desativado");
   inserePlacar();
-
 }
-
-function inicializaMarcadores(){
-  var frase = $(".frase").text();
-  campo.on("input",function(){
-    var digitado = campo.val();
-    var comparavel = frase.substr(0, digitado.length);
-        
-    if(digitado == comparavel){
-      campo.addClass("borda-verde");
-      campo.removeClass("borda-vermelha");
-    }else{
-      campo.addClass("borda-vermelha");
-      campo.removeClass("borda-verde");
-    }
-  });
-}
-
-function inserePlacar(){
-  var corpoTabela = $(".placar").find("tbody");
-  var usuario = "Seu-nome";
-  var numPalavras = $("#contador-palavras").text();
-  var botaoRemover = "<a href='#'><i class = 'small material-icons'>delete</i></a>"
-
-  var linha = "<tr>"+
-                  "<td>"+ usuario + "</td>"+
-                  "<td>"+ numPalavras + "</td>"+
-                  "<td>"+ botaoRemover + "</td>"+
-              "</tr>";
-
-  corpoTabela.prepend(linha);
-}
-
-$(".botao-remover").click(function(event){
-  event.preventDefault();
-  $(this).parent().parent().remove();
-})
 
 function reiniciaJogo(){
   campo.attr("disabled",false);
   campo.val("");
-  $("#contador-palavras").text("0");
-  $("#contador-caracteres").text("0");
-  $("#tempo-digitacao").text(tempoInicial);
+  $("#contador-palavras").text(0);
+  $("#contador-caracteres").text(0);
+  $("#tempo-digitacao").text(tempoInicial);  
   inicializaCronometro();
   campo.toggleClass("campo-desativado");
   campo.removeClass("borda-vermelha");
